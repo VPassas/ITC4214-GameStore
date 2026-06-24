b1cd1f9470e215dace961edadbf9b5e76eda4c32
VPassas <e.passas@acg.edu>
2026-06-24 18:13:13 +0300

    Namespace URLs and moved base layout to a different, global template folder

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+# refer to these urls as "store:home" and "store:game_detail"  to avoid name collisions if another app also has a "home" or "game_detail"
+app_name = "store"
+
 urlpatterns = [
     path("", views.home, name="home"),
+    path("game/<int:id>/", views.game_detail, name="game_detail"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Game
 
 
 def home(request):
-    """the home page, which shows all the games in the store"""
+    """the homepage gets all the games and sends them to the template"""
     games = Game.objects.all()
     return render(request, "store/home.html", {"games": games})
+
+
+def game_detail(request, id):
+    """this shows the details of a single game, identified by its id"""
+    game = get_object_or_404(Game, id=id)
+    return render(request, "store/game_detail.html", {"game": game})
